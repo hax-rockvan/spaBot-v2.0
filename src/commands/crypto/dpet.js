aeafc17f8a810dc0d805d6f6ceb1ec4a19b7c03d
hax-rockvan <hax-rockvan@gmail.com>
2021-06-17 21:28:17 +0800

    added dpet coin info

---
+++ src/commands/crypto/dpet.js
@@ -0,0 +1,59 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+  name: 'dpet',
+  description: 'Shows Current DPET to PHP Rate',
+
+  async execute(message, args) {
+    fetch('https://api.coingecko.com/api/v3/coins/my-defi-pet')
+      .then((res) => res.json())
+      .then(async (dpetData) => {
+        const dpet = await dpetData;
+
+        const dpetEmbed = new Discord.MessageEmbed()
+
+          .setTitle(dpet.name)
+          .setAuthor(
+            'CoinGecko',
+            'https://static.coingecko.com/s/thumbnail-007177f3eca19695592f0b8b0eabbdae282b54154e1be912285c9034ea6cbaf2.png'
+          )
+          .setURL('https://www.coingecko.com/en/coins/my-defi-pet/php')
+          .setThumbnail(
+            'https://assets.coingecko.com/coins/images/15321/small/mydefi.PNG?1620512455'
+          )
+          .setTimestamp()
+          .setFooter('To The Moon!')
+          .setDescription('Current DPET to PHP Rate')
+
+          .addField(
+            'DPET',
+            Intl.NumberFormat('en-PH', {
+              style: 'currency',
+              currency: 'PHP',
+            }).format(dpet.market_data.current_price.php),
+            true
+          )
+          .addField(
+            'Price Change Percentage 24h',
+            `${dpet.market_data.price_change_percentage_24h_in_currency.php} %`,
+            true
+          )
+          .addField(
+            '24h Low / 24h High',
+            `${Intl.NumberFormat('en-PH', {
+              style: 'currency',
+              currency: 'PHP',
+            }).format(dpet.market_data.low_24h.php)} / ${Intl.NumberFormat(
+              'en-PH',
+              {
+                style: 'currency',
+                currency: 'PHP',
+              }
+            ).format(dpet.market_data.high_24h.php)}`
+          );
+
+        message.channel.send(dpetEmbed);
+      });
+  },
+};
