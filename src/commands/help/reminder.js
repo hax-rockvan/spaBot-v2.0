37030a5cffe96e5b33789bea36e846f75c23766c
hax-rockvan <hax-rockvan@gmail.com>
2021-07-06 10:50:10 +0800

    updated reminder

--- src/commands/help/reminder.js
+++ src/commands/help/reminder.js
@@ -10,6 +10,8 @@ module.exports = {
       .setTitle('Important Reminder')
       .addFields(
         { name: 'Iohannes', value: 'Borrowed 0.0023 WETH' },
+        { name: 'Imp', value: 'Pay 500php to Iohannes' },
+        { name: 'Sexty', value: 'Pay 500php to Iohannes' },
         { name: 'Claim SLP', value: 'Jul 19th 2021, 12:23am' },
         { name: 'Refresh Bot', value: 'July 7' }
       );
